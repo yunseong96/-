c950708e920e588384669adb8cb0a86b25db352f
yunseong96 <62291673+yunseong96@users.noreply.github.com>
2020-03-18 12:37:11 +0900

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,12 @@
-<?php
-session_start();
-$res=session_destroy();
-if($res)
-{
-  header('Location: ./login.html');
-}
- ?>
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script>
+      alert('로그아웃 됐습니다.');
+      document.location.href="login.html";
+    </script>
+  </body>
+</html>
